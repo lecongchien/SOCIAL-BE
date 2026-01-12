61b61321d180975168e044e143bcd3847f5e11e1
lecongchien <lecongchien2472002@gmail.com>
2026-01-12 23:35:06 +0700

    Add dotenv and mongodb dependencies; implement database service and user schema

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const name: string = "Dư Thanh Được";
-console.log(name);
+import express from "express";
+import databateService from "./services/databate.services";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use(express.json());
+databateService.connect();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/services/databate.services.ts
@@ -0,0 +1,32 @@
+import { Collection, Db, MongoClient } from "mongodb";
+import { config } from "dotenv";
+import User from "~/model/schemas/User.schema";
+config();
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/?appName=database`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+class DatabateService {
+  private client: MongoClient;
+  private db: Db;
+  constructor() {
+    this.client = new MongoClient(uri);
+    this.db = this.client.db(process.env.DB_NAME);
+  }
+
+  async connect() {
+    try {
+      await this.db.command({ ping: 1 });
+    } finally {
+      await client.close();
+    }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.USERS_COLLECTION as string);
+  }
+}
+
+const databateService = new DatabateService();
+export default databateService;
